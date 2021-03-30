dcb18fbe0e20d7904ad0cf8abe7b3fdc6c051700
Arne <arne@familie-schild.info>
2021-03-30 08:20:21 +0200

    Initial commit

---
+++ addons_gen.cmd
@@ -0,0 +1,4 @@
+copy ..\plugin.image.mypicsdb2\addon.xml plugin.image.mypicsdb2
+rem copy ..\script.service.mypicsdb2\addon.xml script.service.mypicsdb2
+
+"C:\Program Files\Python39\python.exe" addons_gen.py
