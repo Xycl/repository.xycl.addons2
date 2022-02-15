3af4d998629fd06739a8af7c8f62e53c6713d349
Xycl <xycl2008@gmail.com>
2022-02-15 12:59:20 +0100

    19.4.0 New Scanner & Library

--- addons_gen.cmd
+++ addons_gen.cmd
@@ -1,4 +1,6 @@
 copy ..\plugin.image.mypicsdb2\addon.xml plugin.image.mypicsdb2
 copy ..\script.service.mypicsdb2\addon.xml script.service.mypicsdb2
+copy ..\script.module.mypicsdb2lib\addon.xml script.module.mypicsdb2lib
+copy ..\script.module.mypicsdb2scan\addon.xml script.module.mypicsdb2scan
 
 "C:\Program Files\Python39\python.exe" addons_gen.py
